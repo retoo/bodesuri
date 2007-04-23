99aee8e113761a907b55976e62b7848cafed24ee
Robin Stocker <robin@nibor.org>
2007-04-23 14:28:37 +0200

    - Client: Name einlesen und bei Server registrieren.
    svn path=/trunk/; revision=66

--- src/spielplatz/Client.java
+++ src/spielplatz/Client.java
@@ -1,41 +1,42 @@
 package spielplatz;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.rmi.AlreadyBoundException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
-import spielplatz.hilfsklassen.ChatNachricht;
 import spielplatz.hilfsklassen.Nachricht;
+import spielplatz.hilfsklassen.Registrierung;
 
 public class Client {
 	private EndPunkt server;
 	private Briefkasten briefkasten;
 
 	private Client(String name) throws RemoteException, NotBoundException, AlreadyBoundException {
-		if (name == null) {
-			name = this.toString();
-		}
+	}
+	
+	private void run() throws RemoteException, IOException, AlreadyBoundException, NotBoundException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
-		briefkasten = new Briefkasten(name, false);
+		System.out.print("Spielername: ");
+		String spielerName = in.readLine();
+		
+		briefkasten = new Briefkasten(spielerName, false);
 		
 		/* handler des servers */
 		server = briefkasten.schlageNach("server");
 		
-		/* dem server mitteilen wer wir sind */
-		briefkasten.registriereBei(server);
-	}
-	
-	private void run() throws RemoteException {		
-		server.sende(new ChatNachricht("Wie gehts grosser Server?"));
+		server.sende(new Registrierung(spielerName));
 		
 		Nachricht n;
-		
-		while ( ( n = briefkasten.getNächsteNachricht()) != null) {
+		while ((n = briefkasten.getNächsteNachricht()) != null) {
 			System.out.println("Client: " + n);
 		}
 	}
 
-	public static void main(String[] args) throws RemoteException, NotBoundException, AlreadyBoundException, InterruptedException {
+	public static void main(String[] args) throws RemoteException, NotBoundException, AlreadyBoundException, InterruptedException, IOException {
 		String name = null;
 		if ( args.length > 0) {
 			name = args[0];
