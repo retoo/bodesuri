062ae76204ed6a13ae0eb93af6bc0943588fabff
Danilo Couto <danilo.couto@ggaweb.ch>
2007-05-03 00:34:26 +0200

    - Erweiterung des Attributs nummer, das für das CLI für die Darstellung verwendet wird
    svn path=/trunk/; revision=147

--- src/pd/spielerverwaltung/Spieler.java
+++ src/pd/spielerverwaltung/Spieler.java
@@ -8,6 +8,7 @@ import spielplatz.EndPunkt;
 public class Spieler {
 	private String name;
 	public EndPunkt endpunkt;
+	public int nummer;
 	
 	private Vector<Figur> figuren = new Vector<Figur>();
 	
@@ -22,15 +23,28 @@ public class Spieler {
 		this(name);
 		this.endpunkt = client;
 	}
+	
+	public Spieler(String name, int nummer){
+		this.name = name;
+		this.nummer = nummer;
+		for (int i = 0; i < 4; ++i) {
+			figuren.add(new Figur(this));
+		}
+	}
 
 	public String getName() {
     	return name;
     }
 	
+	public int getNummer(){
+		return nummer;
+	}
+	
 	public String toString() {
 		return "Spieler " + getName();
 	}
 
+	// Wird für das CLI verwendet, um die Spieler anzeigen zu können
 	public Vector<Figur> getFiguren() {
     	return figuren;
     }
