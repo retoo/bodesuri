ff153c1b014ff3faddbf1950a9b569016770154c
Reto Schüttel <reto@schuettel.ch>
2007-05-06 16:20:49 +0200

    - nun darf er wieder erfolgreich builden
    svn path=/trunk/; revision=161

--- src/spielplatz/hilfsklassen/VerbindungGeschlossen.java
+++ src/spielplatz/hilfsklassen/VerbindungGeschlossen.java
@@ -3,6 +3,8 @@ package spielplatz.hilfsklassen;
 
 public class VerbindungGeschlossen extends Nachricht {
 
+    private static final long serialVersionUID = 1L;
+
 	public VerbindungGeschlossen() {
 		// TODO Auto-generated constructor stub
 	}
--- src/spielplatz/hilfsklassen/VerbindungWegException.java
+++ src/spielplatz/hilfsklassen/VerbindungWegException.java
@@ -2,5 +2,4 @@ package spielplatz.hilfsklassen;
 
 public class VerbindungWegException extends Exception {
     private static final long serialVersionUID = 1L;
-    asdf;
 }
