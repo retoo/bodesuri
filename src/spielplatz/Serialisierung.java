041dcc8e52453f95a107e5785d293de4bd04cf9e
Robin Stocker <robin@nibor.org>
2007-05-06 18:31:15 +0200

    - Schönere Version mit writeReplace() und readResolve() direkt in Feld.  - Braucht leider global erreichbaren Codierer.
    svn path=/trunk/; revision=162

--- src/spielplatz/Serialisierung.java
+++ src/spielplatz/Serialisierung.java
@@ -3,78 +3,34 @@ package spielplatz;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.OutputStream;
+import java.io.ObjectStreamException;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
 class Codierer {
-	private Map<Feld, Integer> vonFeldZuCode;
-	private Map<Integer, Feld> vonCodeZuFeld;
+	/* Gibt's ne bijektive Map? */
+	private static Map<Feld, Integer> vonFeldZuCode;
+	private static Map<Integer, Feld> vonCodeZuFeld;
 	
-	public Codierer() {
+	static {
 		vonFeldZuCode = new HashMap<Feld, Integer>();
 		vonCodeZuFeld = new HashMap<Integer, Feld>();
 	}
 	
-	public static Codierer von(ObjectOutputStream out) {
-		if (!(out instanceof ObjectOutputStreamMitCodierer)) {
-			/* TODO: Exception oder so */
-		}
-		ObjectOutputStreamMitCodierer s = (ObjectOutputStreamMitCodierer) out;
-		return s.getCodierer();
-	}
-	
-	public static Codierer von(ObjectInputStream in) {
-		if (!(in instanceof ObjectInputStreamMitCodierer)) {
-			/* TODO: Exception oder so */
-		}
-		ObjectInputStreamMitCodierer s = (ObjectInputStreamMitCodierer) in;
-		return s.getCodierer();
-	}
-	
-	public void speichere(Feld feld, int code) {
+	public static void speichere(Feld feld, int code) {
 		vonFeldZuCode.put(feld, code);
 		vonCodeZuFeld.put(code, feld);
 	}
 	
-	public Integer getCode(Feld feld) {
-		return vonFeldZuCode.get(feld);
+	public static FeldCode getFeldCode(Feld feld) {
+		return new FeldCode(vonFeldZuCode.get(feld));
 	}
 	
-	public Feld getFeld(int code) {
+	public static Feld getFeld(int code) {
 		return vonCodeZuFeld.get(code);
-	}
-}
-
-class ObjectOutputStreamMitCodierer extends ObjectOutputStream {
-	private Codierer codierer;
-	
-	public ObjectOutputStreamMitCodierer(OutputStream os, Codierer c)
-			throws IOException {
-		super(os);
-		codierer = c;
-	}
-	
-	public Codierer getCodierer() {
-		return codierer;
-	}
-}
-
-class ObjectInputStreamMitCodierer extends ObjectInputStream {
-	private Codierer codierer;
-	
-	public ObjectInputStreamMitCodierer(InputStream is, Codierer c)
-			throws IOException {
-		super(is);
-		codierer = c;
-	}
-	
-	public Codierer getCodierer() {
-		return codierer;
 	}
 }
 
@@ -89,18 +45,6 @@ class Bewegung implements Serializable {
 		this.ziel  = ziel;
     }
 
-	private void writeObject(ObjectOutputStream out) throws IOException {
-		Codierer c = Codierer.von(out);
-		out.write(c.getCode(start));
-		out.write(c.getCode(ziel));
-	}
-
-	private void readObject(ObjectInputStream in) throws IOException {
-		Codierer c = Codierer.von(in);
-		start = c.getFeld(in.read());
-		ziel  = c.getFeld(in.read());
-	}
-
 	public Feld getStart() {
 		return start;
 	}
@@ -110,43 +54,51 @@ class Bewegung implements Serializable {
 	}
 }
 
-class Feld {
+class Feld implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	Object writeReplace() throws ObjectStreamException {
+		return Codierer.getFeldCode(this);
+	}
+}
+
+class FeldCode implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private int code;
+	
+	public FeldCode(int code) {
+		this.code = code;
+	}
+	
+	Object readResolve() throws ObjectStreamException {
+		return Codierer.getFeld(code);
+	}
 }
 
 public class Serialisierung {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+			throws IOException, ClassNotFoundException {
 		Feld start = new Feld();
 		Feld ziel  = new Feld();
 		
-		Codierer codierer = new Codierer();
-		codierer.speichere(start, 0);
-		codierer.speichere(ziel, 1);
+		Codierer.speichere(start, 0);
+		Codierer.speichere(ziel, 1);
 		
 		Bewegung original = new Bewegung(start, ziel);
 		
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		ObjectOutputStreamMitCodierer oos;
-		try {
-			oos = new ObjectOutputStreamMitCodierer(baos, codierer);
-			oos.writeObject(original);
-			oos.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-        }
+		ObjectOutputStream oos;
+		oos = new ObjectOutputStream(baos);
+		oos.writeObject(original);
+		oos.close();
 
 		byte[] ausgabe = baos.toByteArray();
 
 		ByteArrayInputStream bais = new ByteArrayInputStream(ausgabe);
-		ObjectInputStreamMitCodierer ois;
-		Bewegung neu = null;
-		try {
-			ois = new ObjectInputStreamMitCodierer(bais, codierer);
-			neu = (Bewegung) ois.readObject();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
+		ObjectInputStream ois;
+		ois = new ObjectInputStream(bais);
+		Bewegung neu = (Bewegung) ois.readObject();
 
 		System.out.println(ausgabe.length);
 		System.out.println(start == neu.getStart());
