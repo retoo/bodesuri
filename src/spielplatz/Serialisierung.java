8cdac08ff3efdb30d30e638d5ce5ae62e1262dc9
Robin Stocker <robin@nibor.org>
2007-05-07 23:04:42 +0200

    - Noch eine Variante der Serialisierung, die mit einer HashMap<String, CodierbaresObjekt> arbeitet.
    svn path=/trunk/; revision=177

--- src/spielplatz/Serialisierung.java
+++ src/spielplatz/Serialisierung.java
@@ -11,26 +11,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Codierer {
-	/* Gibt's ne bijektive Map? */
-	private static Map<Feld, Integer> vonFeldZuCode;
-	private static Map<Integer, Feld> vonCodeZuFeld;
-	
+	private static Map<String, CodierbaresObjekt> objekte;
 	static {
-		vonFeldZuCode = new HashMap<Feld, Integer>();
-		vonCodeZuFeld = new HashMap<Integer, Feld>();
+		objekte = new HashMap<String, CodierbaresObjekt>();
 	}
 	
-	public static void speichere(Feld feld, int code) {
-		vonFeldZuCode.put(feld, code);
-		vonCodeZuFeld.put(code, feld);
+	public static void speichere(CodierbaresObjekt objekt) {
+		objekte.put(objekt.getCode(), objekt);
 	}
 	
-	public static FeldCode getFeldCode(Feld feld) {
-		return new FeldCode(vonFeldZuCode.get(feld));
-	}
-	
-	public static Feld getFeld(int code) {
-		return vonCodeZuFeld.get(code);
+	public static Object get(String code) {
+		return objekte.get(code);
 	}
 }
 
@@ -54,36 +45,50 @@ class Bewegung implements Serializable {
 	}
 }
 
-class Feld implements Serializable {
-	private static final long serialVersionUID = 1L;
-
-	private Object writeReplace() throws ObjectStreamException {
-		return Codierer.getFeldCode(this);
+abstract class CodierbaresObjekt implements Serializable {
+	abstract public String getCode();
+	
+	protected Object writeReplace() throws ObjectStreamException {
+		return new CodiertesObjekt(getCode());
 	}
 }
 
-class FeldCode implements Serializable {
+class Feld extends CodierbaresObjekt {
 	private static final long serialVersionUID = 1L;
 	
-	private int code;
+	private int nummer;
 	
-	public FeldCode(int code) {
+	public Feld(int nummer) {
+		this.nummer = nummer;
+	}
+	
+	public String getCode() {
+		return "Feld " + nummer;
+	}
+}
+
+class CodiertesObjekt implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String code;
+	
+	public CodiertesObjekt(String code) {
 		this.code = code;
 	}
 	
 	private Object readResolve() throws ObjectStreamException {
-		return Codierer.getFeld(code);
+		return Codierer.get(code);
 	}
 }
 
 public class Serialisierung {
 	public static void main(String[] args)
 			throws IOException, ClassNotFoundException {
-		Feld start = new Feld();
-		Feld ziel  = new Feld();
+		Feld start = new Feld(0);
+		Feld ziel  = new Feld(1);
 		
-		Codierer.speichere(start, 0);
-		Codierer.speichere(ziel, 1);
+		Codierer.speichere(start);
+		Codierer.speichere(ziel);
 		
 		Bewegung original = new Bewegung(start, ziel);
 		
