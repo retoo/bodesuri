465f18f8f29de4e21fa333e55f7d41a217b9d0dd
Robin Stocker <robin@nibor.org>
2007-05-13 20:42:59 +0200

    - RueckwaertsRegel leitet nun von VorwaertsRegel ab und überschreibt nur noch getWeg.
    svn path=/trunk/; revision=252

--- src/pd/regelsystem/RueckwaertsRegel.java
+++ src/pd/regelsystem/RueckwaertsRegel.java
@@ -1,37 +1,24 @@
 package pd.regelsystem;
 
+import java.util.List;
+import java.util.Vector;
+
 import pd.brett.Feld;
-import pd.zugsystem.Aktion;
 import pd.zugsystem.Bewegung;
-import pd.zugsystem.Zug;
-import pd.zugsystem.ZugEingabe;
 
-public class RueckwaertsRegel extends Regel {
-	private int schritte;
-	
+public class RueckwaertsRegel extends VorwaertsRegel {
 	public RueckwaertsRegel(int schritte) {
-		this.schritte = schritte;
+		super(schritte);
 	}
-
-	public Zug validiere(ZugEingabe zugEingabe) throws RegelVerstoss {
-		Bewegung bewegung = zugEingabe.getBewegung();
-		Feld start = bewegung.getStart();
-		Feld ziel  = bewegung.getZiel();
-		
-		if (!start.istBesetztVon(zugEingabe.getSpieler())) {
-			throw new RegelVerstoss("Zug muss mit Figur begonnen werden.");
+	
+	protected List<Feld> getWeg(Bewegung bewegung) {
+		Vector<Feld> weg = new Vector<Feld>();
+		Feld feld = bewegung.getStart();
+		while (feld != bewegung.getZiel()) {
+			weg.add(feld);
+			feld = feld.getVorheriges();
 		}
-		
-		if (start.getWegRueckwaerts(ziel).size()-1 != schritte) {
-			throw new RegelVerstoss("Zug muss über " + schritte +
-			                        " Felder gehen.");
-		}
-		
-		Zug zug = new Zug();
-		zug.fuegeHinzu(new Aktion(start, ziel));
-		
-		/* TODO: Aktion für Figur auf Ziel heimschicken. */
-		
-		return zug;
+		weg.add(feld);
+		return weg;
 	}
 }
--- src/pd/regelsystem/VorwaertsRegel.java
+++ src/pd/regelsystem/VorwaertsRegel.java
@@ -1,5 +1,8 @@
 package pd.regelsystem;
 
+import java.util.List;
+import java.util.Vector;
+
 import pd.brett.Feld;
 import pd.zugsystem.Aktion;
 import pd.zugsystem.Bewegung;
@@ -12,18 +15,17 @@ public class VorwaertsRegel extends Regel {
 	public VorwaertsRegel(int schritte) {
 		this.schritte = schritte;
 	}
-
+	
 	public Zug validiere(ZugEingabe zugEingabe) throws RegelVerstoss {
 		Bewegung bewegung = zugEingabe.getBewegung();
 		Feld start = bewegung.getStart();
 		Feld ziel  = bewegung.getZiel();
 		
-		/* TODO: Ist das bei allen Regeln so? -> Auslagern */
 		if (!start.istBesetztVon(zugEingabe.getSpieler())) {
 			throw new RegelVerstoss("Zug muss mit Figur begonnen werden.");
 		}
 		
-		if (start.getWeg(ziel).size() != schritte + 1) {
+		if (getWeg(bewegung).size() != schritte + 1) {
 			throw new RegelVerstoss("Zug muss über " + schritte +
 			                        " Felder gehen.");
 		}
@@ -34,5 +36,16 @@ public class VorwaertsRegel extends Regel {
 		/* TODO: Aktion für Figur auf Ziel heimschicken. */
 		
 		return zug;
+	}
+	
+	protected List<Feld> getWeg(Bewegung bewegung) {
+		Vector<Feld> weg = new Vector<Feld>();
+		Feld feld = bewegung.getStart();
+		while (feld != bewegung.getZiel()) {
+			weg.add(feld);
+			feld = feld.getNaechstes();
+		}
+		weg.add(feld);
+		return weg;
 	}
 }
