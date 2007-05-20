104160b7812d3492363b3d17bed8b41c1e882ef8
Philippe Eberli <p.eberli@gmx.ch>
2007-05-20 12:45:23 +0200

    - Bug in der While-Schlaufe behoben (das letzte Element hat immer gefehlt)
    svn path=/trunk/; revision=318

--- src/ui/brett/BrettView.java
+++ src/ui/brett/BrettView.java
@@ -40,44 +40,19 @@ public class BrettView extends JPanel {
 		FelderXML felderXML = new FelderXML();
 
 		Feld feld = startFeld;
-		int i = 1;
-		while (feld != startFeld.getVorheriges()) {
+		int i = 0;
+		do {
 			Feld2d feld2d = new Feld2d(felderXML.getKoordinaten(i));
 			felder.put(feld, feld2d);
+			System.out.println(i);
+			System.out.println(feld);
 			if (feld.istBesetzt()) {
 				new Figur2d(feld2d, this);
 			}
 
 			feld = feld.getNaechstes();
 			i++;
-		}
-		
-		/*for (int i = 0; i < 4; i++) {
-		
-		while (felderXML.hasNext()) {
-			Point point = new Point();
-			Element feldXML = (Element) felderXML.getNext();
-			if (i == 0) {
-				point.x = felderXML.getX(feldXML);
-				point.y = felderXML.getY(feldXML);
-			}
-			if (i == 1) {
-				point.x = felderXML.getY(feldXML);
-				point.y = 600 - felderXML.getX(feldXML);
-			}
-			if (i == 2) {
-				point.x = 600 - felderXML.getX(feldXML);
-				point.y = 600 - felderXML.getY(feldXML);
-			}
-			if (i == 3) {
-				point.x = 600 - felderXML.getY(feldXML);
-				point.y = felderXML.getX(feldXML);
-			}
-
-			Feld2d feld2d = new Feld2d(point);
-			felder.put(feld, feld2d);
-			}
-		}*/
+		} while (feld != startFeld);
 
 		for (Feld2d feld2 : felder.values()) {
 			this.add(feld2);
