50fef39faaa96accd96b4f96d99f735ee1e16097
Philippe Eberli <p.eberli@gmx.ch>
2007-05-22 23:27:28 +0200

    - Leere Lobby erstellt
    svn path=/trunk/; revision=403

---
+++ src/ui/lobby/LobbyView.java
@@ -0,0 +1,12 @@
+package ui.lobby;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class LobbyView extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public LobbyView() {
+		add(new JLabel("Test"));
+	}
+}
