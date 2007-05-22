51f264f74cb32aa3d9080c946f6091f5f9952518
Philippe Eberli <p.eberli@gmx.ch>
2007-05-22 21:36:51 +0200

    - Vector<Feld2d> & dazugehöriges foreach duch Figur2d mit höherem z-Index ersetzt.
    svn path=/trunk/; revision=392

--- src/ui/brett/BrettView.java
+++ src/ui/brett/BrettView.java
@@ -3,9 +3,7 @@ package ui.brett;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
-import java.util.List;
 import java.util.Map;
-import java.util.Vector;
 
 import javax.swing.JPanel;
 
@@ -19,10 +17,6 @@ import dienste.netzwerk.EndPunkt;
 public class BrettView extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	// Wenn wir von Feld nach Feld2d und umgekehrt abbilden wollen:
-	// HashIdentityMap
-	// Evtl. auch noch eine Struktur von Figur2d
-	List<Feld2d> felder = new Vector<Feld2d>();
 
 	public BrettView(Spiel spiel, Spieler spielerIch, EndPunkt server) {
 		// Nur vorübergehend, damit man sehen kann wie gross das Panel ist...
@@ -42,17 +36,11 @@ public class BrettView extends JPanel {
 
 		for (Feld feld : spiel.getBrett().getAlleFelder()) {
 			Feld2d feld2d = new Feld2d(koordinaten.get(feld.getNummer()), feld);
-			felder.add(feld2d);
+			this.add(feld2d);
 			System.out.println(feld);
 			if (feld.istBesetzt()) {
-				this.add(new Figur2d(feld2d, this));
+				this.setComponentZOrder(new Figur2d(feld2d, this), 0);
 			}
-		}
-
-		// TODO Diese For-Schaufe sollte durch setComponentZOrder anstelle von
-		// add obsolet werden
-		for (Feld2d feld2 : felder) {
-			this.add(feld2);
 		}
 	}
 
