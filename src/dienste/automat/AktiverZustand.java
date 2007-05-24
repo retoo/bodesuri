cf1ff7ebc527874947f379e8581089730758e267
Philippe Eberli <p.eberli@gmx.ch>
2007-05-24 10:36:40 +0200

    - AktiverZustand gibt im Fehlerfall auch an welcher Event aufgetreten ist.
    svn path=/trunk/; revision=429

--- src/dienste/automat/AktiverZustand.java
+++ src/dienste/automat/AktiverZustand.java
@@ -2,6 +2,6 @@ package dienste.automat;
 
 public abstract class AktiverZustand extends Zustand {
 	public Zustand handle(Event event) {
-		throw new UnbekannterEventException("Zustand " + this + " hat keinen Übergang für Event.");
+		throw new UnbekannterEventException("Zustand " + this + " hat keinen Übergang für den Event " + event.getClass());
     }
 }
