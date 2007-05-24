e8a5757f7aa35bfdcae113f78936f38810dd434f
Philippe Eberli <p.eberli@gmx.ch>
2007-05-24 15:48:51 +0200

    - Züge von anderen Spieler werden verarbeitet
    svn path=/trunk/; revision=436

--- src/applikation/client/zustaende/NichtAmZug.java
+++ src/applikation/client/zustaende/NichtAmZug.java
@@ -1,7 +1,9 @@
 package applikation.client.zustaende;
 
+import pd.regelsystem.RegelVerstoss;
 import applikation.server.nachrichten.ChatNachricht;
 import applikation.server.nachrichten.ZugAufforderung;
+import applikation.server.nachrichten.ZugInformation;
 import dienste.automat.Zustand;
 import dienste.netzwerk.EndPunkt;
 
@@ -14,5 +16,15 @@ public class NichtAmZug extends AktiverClientZustand {
 	//TODO: Brauchen wir die Nachricht übrerhaupt?
 	Zustand zugAufforderung(ZugAufforderung aufforderung) {
 		return automat.getZustand(AmZug.class);
+	}
+	
+	Zustand zugWurdeGemacht(ZugInformation information) {
+		try {
+	        information.zug.validiere().ausfuehren();
+        } catch (RegelVerstoss e) {
+        	System.out.println("Ungültigen Zug (" + e + ") vom Server erhalten!");
+        	return automat.getZustand(SchwererFehler.class);		
+        }
+		return this;
 	}
 }
