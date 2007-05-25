efb6db7aa34cf730475776dd14b618af124d549d
Reto Schüttel <reto@schuettel.ch>
2007-05-25 22:08:39 +0200

    - run() Aufruf des Clients in ein try/catch eingepackt. Fehler werden jetzt abefangen und führen zu einem exit(99(
    svn path=/trunk/; revision=454

--- src/ui/ClientController.java
+++ src/ui/ClientController.java
@@ -12,8 +12,15 @@ public class ClientController {
 	public static void main(String[] args) throws UnknownHostException,
 	                                      IOException, VerbindungWegException {
 		EventQueue queue = new EventQueue();
-		Automat sync = new BodesuriClient(queue);
+		Automat automat = new BodesuriClient(queue);
 
-		sync.run();
+		try {
+			automat.run();
+		} catch (Exception e) {
+			/* Applikation stoppen wenn ein Fehler auftritt */
+			e.printStackTrace();
+			System.out.println("Client: Exception in run()");
+			System.exit(99);
+		}
 	}
 }
