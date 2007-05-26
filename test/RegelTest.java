d9b059ad183a6b291d473a1c3ec2b54207c31c45
Robin Stocker <robin@nibor.org>
2007-05-27 00:59:50 +0200

    - Mehr Tests! Ich will 100% :)
    svn path=/trunk/; revision=489

--- test/RegelTest.java
+++ test/RegelTest.java
@@ -42,6 +42,8 @@ public class RegelTest extends ProblemDomainTestCase {
 		lagerFeld1.versetzeFigurAuf(start);
 		
 		sollteVerstossGeben(viererRegel);
+		start = lagerFeld1;
+		sollteVerstossGeben(viererRegel);
 	}
 
 	public void testVierRueckwaerts() throws RegelVerstoss {
@@ -71,6 +73,33 @@ public class RegelTest extends ProblemDomainTestCase {
 		
 		assertTrue(start.istFrei());
 		assertTrue(ziel.istBesetztVon(spieler1));
+	}
+	
+	public void testStartNichtVonLagerFeldAus() {
+		start = bankFeld1;
+		ziel  = bankFeld1.getNaechstes();
+		sollteVerstossGeben(new StartRegel());
+	}
+	
+	public void testStartKeineFigurAufLagerFeld() {
+		start = lagerFeld1;
+		ziel  = bankFeld1;
+		lagerFeld1.versetzeFigurAuf(ziel);
+		sollteVerstossGeben(new StartRegel());
+	}
+	
+	public void testStartNichtAufBankFeld() {
+		start = lagerFeld1;
+		ziel  = bankFeld1.getNaechstes();
+		sollteVerstossGeben(new StartRegel());
+		ziel  = bankFeld2;
+		sollteVerstossGeben(new StartRegel());
+	}
+	
+	public void testStartAndererSpieler() {
+		start = lagerFeld2;
+		ziel  = bankFeld2;
+		sollteVerstossGeben(new StartRegel());
 	}
 	
 	public void testHeimSchicken() throws RegelVerstoss {
@@ -114,6 +143,15 @@ public class RegelTest extends ProblemDomainTestCase {
 		start.setFigur(spieler2.getFiguren().get(0));
 		ziel.setFigur(spieler2.getFiguren().get(1));
 		
+		sollteVerstossGeben(new TauschRegel());
+	}
+	
+	public void testTauschenMitEinerFigur() {
+		start = bankFeld1;
+		ziel  = bankFeld1.getNaechstes();
+		lagerFeld1.versetzeFigurAuf(start);
+		sollteVerstossGeben(new TauschRegel());
+		start.versetzeFigurAuf(ziel);
 		sollteVerstossGeben(new TauschRegel());
 	}
 	
