cff6904e91d5866d70e5932da3a169e2b8353778
Reto Schüttel <reto@schuettel.ch>
2007-05-30 23:51:25 +0200

    - Zustände so angepasst dass man jetzt damit testen kann
    svn path=/trunk/; revision=652

--- src/applikation/client/zustaende/KarteTauschenAuswaehlen.java
+++ src/applikation/client/zustaende/KarteTauschenAuswaehlen.java
@@ -1,5 +1,9 @@
 package applikation.client.zustaende;
 
-public class KarteTauschenAuswaehlen extends AktiverClientZustand {
+import dienste.automat.zustaende.Zustand;
 
+public class KarteTauschenAuswaehlen extends PassiverClientZustand {
+    public Zustand handle() {
+	    return automat.getZustand(KarteTauschenBekommen.class);
+    }
 }
