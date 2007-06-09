585aa83843f033928892e62714827368a4348902
Reto Schüttel <reto@schuettel.ch>
2007-06-10 00:35:15 +0200

    - Kommentar an Philippe/Danilo bezgülich mouse-adapter
    svn path=/trunk/; revision=800

--- src/ui/spiel/brett/FeldMouseAdapter.java
+++ src/ui/spiel/brett/FeldMouseAdapter.java
@@ -25,25 +25,26 @@ public class FeldMouseAdapter extends MouseAdapter {
 	 * Das {@link Feld2d} auf das geklickt wird an den Controller weiterleiten
 	 * und falls eine {@link Figur2d} auf dem Feld ist, diese als ausgewählt
 	 * markieren.
-	 * 
+	 *
 	 * @param e
 	 *            MouseEvent der das angeklickte Feld enthält
 	 */
 	public void mouseClicked(MouseEvent e) {
 		if (aktiv) {
 			controller.feldGewaehlt(((Feld2d) e.getComponent()).feld);
-			
+
 			// TODO: Falls die Figur schon gewählt ist, wieder ent-wählen
+			// TODO: sollten solche Logik nicht im automaten passieren? (-reto)
 			Figur2d figur = getFigur2d(((Feld2d) e.getComponent()));
 			if (figur != null) {
 				figur.setzeAusgewaehlt();
-			}	
+			}
 		}
 	}
 
 	public void addFigur(Feld2d feld2d, Figur2d figur2d) {
 		this.felder.put(feld2d, figur2d);
-		
+
 	}
 
 	public Figur2d getFigur2d(Feld2d feld2d) {
