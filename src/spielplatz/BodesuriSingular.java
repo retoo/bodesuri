9fb42f1c3d4da0373ced1e59887f6d76b9bea4e0
Reto Schüttel <reto@schuettel.ch>
2007-06-11 15:21:36 +0200

    - Bodeusri Singular
    svn path=/trunk/; revision=849

---
+++ src/spielplatz/BodesuriSingular.java
@@ -0,0 +1,44 @@
+package spielplatz;
+
+import ui.GUIController;
+import applikation.client.ClientAutomat;
+import applikation.client.controller.Controller;
+import applikation.server.ServerAutomat;
+import dienste.automat.Automat;
+
+public class BodesuriSingular {
+
+	public static void main(String[] args) {
+		Thread server = new Thread(new Runnable() {
+			public void run() {
+				ServerAutomat server = new ServerAutomat(1);
+
+				try {
+					server.run();
+				} catch (Exception e) {
+					e.printStackTrace();
+					System.out.println("Server: Exception in run()");
+					System.exit(99);
+				}
+            }
+		});
+
+		server.start();
+
+
+		//EventQueue eventQueue = new EventQueue();
+		Controller controller = new GUIController("Singular");
+		Automat client = new ClientAutomat(controller);
+
+		controller. verbinde("localhost", 7788, "Singular");
+
+
+		try {
+			client.run();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Client: Exception in run()");
+			System.exit(99);
+		}
+	}
+}
