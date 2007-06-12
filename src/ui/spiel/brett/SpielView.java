35b97d5d185406bbd10c9bbcf653b4b88ed1221b
Philippe Eberli <p.eberli@gmx.ch>
2007-06-12 17:52:20 +0200

    - Icon-Erstellung ausgelagert, damit es nicht bei jedem Update neu berechnet werden muss
    svn path=/trunk/; revision=890

--- src/ui/spiel/brett/SpielView.java
+++ src/ui/spiel/brett/SpielView.java
@@ -2,6 +2,7 @@ package ui.spiel.brett;
 
 import java.awt.BorderLayout;
 import java.awt.Graphics;
+import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
@@ -11,7 +12,11 @@ import ui.ressourcen.Icons;
 import ui.spiel.karten.KartenAuswahlView;
 
 public class SpielView extends JPanel {
+	Image filz;
 	public SpielView(GUIController controller) {
+		ImageIcon icon = (ImageIcon) Icons.FILZ;
+		filz = icon.getImage();
+		
 		// Layout setzen
 		setLayout(new BorderLayout());
 		setOpaque(false);
@@ -26,7 +31,7 @@ public class SpielView extends JPanel {
 	}
 	
 	protected void paintComponent(Graphics g) {
-		g.drawImage(((ImageIcon) Icons.FILZ).getImage(), 0, 0, null);
+		g.drawImage(filz, 0, 0, null);
 		super.paintComponent(g);
 	}
 }
