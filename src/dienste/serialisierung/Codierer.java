9d2404fa06eb170d547a763d5f228a57fd5fc4d1
Reto Schüttel <reto@schuettel.ch>
2007-06-14 13:29:44 +0200

    - Serialisierung ist nicht mehr zyklicsh
    svn path=/trunk/; revision=944

--- src/dienste/serialisierung/Codierer.java
+++ src/dienste/serialisierung/Codierer.java
@@ -6,45 +6,45 @@ import java.util.Map;
 /**
  * Codierer, den es pro {@link pd.Spiel} einmal gibt und der die Abbildung von
  * Codes zu Objekten verwaltet.
- * 
+ *
  * Jedes codierbare Objekt hat einen eindeutigen Code zugeordnet. Wenn das
  * Objekt serialisiert wird, wird nicht das Objekt selber geschrieben, sondern
  * ein codiertes Objekt (ein Ersatzobjekt) mit einem Code.
- * 
+ *
  * Das codierte Objekt wird übermittelt und auf einem anderen Rechner wird es
  * decodiert, indem in der lokalen Tabelle das zu diesem Code gespeicherte
  * Objekt hervorgeholt wird.
- * 
+ *
  * Das urprünglich versendete Objekt entspricht dem auf dem Empfänger, da die
  * Tabelle gleich aufgebaut wurde.
  */
 public class Codierer {
-	private Map<String, CodierbaresObjekt> objekte;
-	
+	private Map<String, Object> objekte;
+
 	/**
 	 * Erstellt einen Codierer.
 	 */
 	public Codierer() {
-		objekte = new HashMap<String, CodierbaresObjekt>();
+		objekte = new HashMap<String, Object>();
 	}
-	
+
 	/**
 	 * Speichert einen Code und das zugehörige codierbare Objekt.
-	 * 
+	 *
 	 * @param code Eindeutiger Code des Objekts
 	 * @param objekt Zugehöriges Objekt, welches codierbar ist
 	 */
-	public void speichere(String code, CodierbaresObjekt objekt) {
+	public void speichere(String code, Object objekt) {
 		objekte.put(code, objekt);
 	}
-	
+
 	/**
 	 * Gibt von einem Code das zugehörige Objekt zurück.
-	 * 
+	 *
 	 * @param code Code des Objekts
 	 * @return Objekt, das mit dem Code gespeicher wurde
 	 */
-	public CodierbaresObjekt get(String code) {
+	public Object get(String code) {
 		return objekte.get(code);
 	}
 }
