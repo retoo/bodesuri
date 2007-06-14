62ba796a4ef819c482afa5f1f30cb428d907f57a
Philippe Eberli <p.eberli@gmx.ch>
2007-06-14 18:17:44 +0200

    app.Spieler vereinfacht (mir ist aus Versehen Dekorator-Code reingerutscht)
    svn path=/trunk/; revision=954

--- src/applikation/client/pd/Spieler.java
+++ src/applikation/client/pd/Spieler.java
@@ -2,27 +2,13 @@ package applikation.client.pd;
 
 import java.util.Observable;
 
-import pd.karten.Karte;
-
-import dienste.observer.ObservableList;
-
 public class Spieler extends Observable {
 	private Boolean amZug;
 	public pd.spieler.Spieler spieler;
 
-	private ObservableList<Karte> karten = new ObservableList<Karte>();
-
 	public Spieler(pd.spieler.Spieler spieler) {
 		this.spieler = spieler;
 		this.amZug = false;
-	}
-
-	public boolean kannZiehen() {
-		return spieler.kannZiehen();
-	}
-
-	public ObservableList<Karte> getKarten() {
-		return karten;
 	}
 
 	public Boolean getAmZug() {
