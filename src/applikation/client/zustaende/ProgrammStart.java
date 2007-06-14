e331ae4aa47b2851766b55b1001521d4120a2b73
Philippe Eberli <p.eberli@gmx.ch>
2007-06-14 10:53:13 +0200

    - Warnung entfernt
    svn path=/trunk/; revision=938

--- src/applikation/client/zustaende/ProgrammStart.java
+++ src/applikation/client/zustaende/ProgrammStart.java
@@ -5,7 +5,7 @@ import dienste.automat.zustaende.Zustand;
 
 /**
  * Zustand in welcher dem Automat beim Start ist. Öffnet den
- * {@link VerbindenView} und geht direkt in den Zustand {@link Lobby} über.
+ * VerbindenView und geht direkt in den Zustand {@link Lobby} über.
  */
 public class ProgrammStart extends ClientZustand implements PassiverZustand {
 	public Class<? extends Zustand> handle() {
