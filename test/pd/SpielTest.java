6a3bacaa75750786133a3b26b9997f9b95328941
Reto Schüttel <reto@schuettel.ch>
2007-06-18 10:12:11 +0200

    - so es blöds tool meint das sei besser :)
    svn path=/trunk/; revision=1006

--- test/pd/SpielTest.java
+++ test/pd/SpielTest.java
@@ -4,9 +4,10 @@ import pd.Spiel;
 
 public class SpielTest extends ProblemDomainTestCase {
 	protected void setUp() {
+		super.setUp();
 		spiel = new Spiel();
 	}
-	
+
 	public void testSpieler() {
 		assertEquals(4, spiel.getSpieler().size());
 		spiel.fuegeHinzu("Spieler 0");
@@ -14,7 +15,7 @@ public class SpielTest extends ProblemDomainTestCase {
 		spiel.fuegeHinzu("Spieler 2");
 		spiel.fuegeHinzu("Spieler 3");
 		assertEquals(4, spiel.getSpieler().size());
-		
+
 		assertEquals(0, spieler(0).getNummer());
 		assertEquals("Spieler 0", spieler(0).getName());
 		assertEquals("Spieler Spieler 0", spieler(0).toString());
