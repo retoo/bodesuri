adfc94dc2955e020d25a29ae5810c201c98c01cd
Reto Schüttel <reto@schuettel.ch>
2007-06-18 16:25:03 +0200

    - böhse warnung entfernt
    svn path=/trunk/; revision=1044

--- test/pd/regelsystem/RueckwaertsRegelTest.java
+++ test/pd/regelsystem/RueckwaertsRegelTest.java
@@ -7,41 +7,41 @@ public class RueckwaertsRegelTest extends RegelTestCase {
 		lager(0).versetzeFigurAuf(start);
 		sollteValidieren(new RueckwaertsRegel(5));
 	}
-	
-	public void testRueckwaertsVorwaerts() throws RegelVerstoss {
+
+	public void testRueckwaertsVorwaerts() {
 		start = bank(0);
 		ziel  = bank(0).getNtesFeld(5);
 		lager(0).versetzeFigurAuf(start);
 		sollteVerstossGeben(new RueckwaertsRegel(5));
 	}
-	
+
 	public void testRueckwaertsImHimmel() {
 		start = himmel(0, 3);
 		ziel  = himmel(0, 1);
 		lager(0).versetzeFigurAuf(start);
 		sollteVerstossGeben(new RueckwaertsRegel(2));
 	}
-	
+
 	public void testRueckwaertsAusHimmel() {
 		start = himmel(0).getNaechstes();
 		ziel  = bank(0);
 		lager(0).versetzeFigurAuf(start);
 		sollteVerstossGeben(new RueckwaertsRegel(2));
 	}
-	
+
 	public void testRueckwaertsInHimmel() {
 		start = bank(0).getNaechstes();
 		ziel  = himmel(0);
 		lager(0).versetzeFigurAuf(start);
 		sollteVerstossGeben(new RueckwaertsRegel(2));
 	}
-	
+
 	public void testRueckwaertsAusLager() {
 		start = lager(0);
 		ziel  = bank(0);
 		sollteVerstossGeben(new RueckwaertsRegel(1));
 	}
-	
+
 	public void testRueckwaertsInLager() {
 		start = bank(0);
 		ziel  = lager(0);
