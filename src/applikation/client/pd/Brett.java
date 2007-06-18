dda89e3d3197139fb5e580e552c4ae5f21eb8013
Reto Schüttel <reto@schuettel.ch>
2007-06-18 10:03:08 +0200

    - ungenutztes lokales feld
    svn path=/trunk/; revision=1004

--- src/applikation/client/pd/Brett.java
+++ src/applikation/client/pd/Brett.java
@@ -4,13 +4,9 @@ import java.util.List;
 import java.util.Vector;
 
 public class Brett {
-	pd.brett.Brett brett;
-
 	private Vector<Feld> alleFelder = new Vector<Feld>();
 
 	public Brett (pd.brett.Brett brett) {
-		/* TODO: brett is never read (-reto)*/
-		this.brett = brett;
 		for (pd.brett.Feld feld :  brett.getAlleFelder()) {
 			alleFelder.add(new Feld(feld));
 		}
