521a107f266642feefa871fbee344b25c9b19c5b
Reto Schüttel <reto@schuettel.ch>
2007-06-19 13:44:33 +0200

    - Mehr Details zur 'Verbindungweg' Exception
    svn path=/trunk/; revision=1077

--- src/dienste/netzwerk/VerbindungWegException.java
+++ src/dienste/netzwerk/VerbindungWegException.java
@@ -1,7 +1,12 @@
 package dienste.netzwerk;
 
+import java.io.IOException;
+
 /**
  * Meldet einen schweren Fehler in einer Netzwerkverbindung.
  */
 public class VerbindungWegException extends RuntimeException {
+	public VerbindungWegException(IOException e) {
+	    super(e);
+    }
 }
