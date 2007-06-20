18516d08b88dbdf9b739e6e31e3729a2dd07fa77
Philippe Eberli <p.eberli@gmx.ch>
2007-06-20 08:54:08 +0200

    - TODO entfernt
    svn path=/trunk/; revision=1105

--- src/applikation/client/zustaende/ProgrammStart.java
+++ src/applikation/client/zustaende/ProgrammStart.java
@@ -7,8 +7,6 @@ import dienste.automat.zustaende.Zustand;
  * Zustand in welcher dem Automat beim Start ist. Geht direkt in den Zustand
  * {@link VerbindungErfassen} über.
  */
-// TODO: Philippe: Diesen könnten wir nun auch löschen. Der Automat funktioniert
-// aber nicht wenn der 1. Zustand aktiv ist.
 public class ProgrammStart extends ClientZustand implements PassiverZustand {
 	public Class<? extends Zustand> handle() {
 		return VerbindungErfassen.class;
