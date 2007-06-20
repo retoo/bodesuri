bebab9122ad2e4cacc81ea21fd87d64bf581b980
Robin Stocker <robin@nibor.org>
2007-06-20 16:04:22 +0200

    - Code von SpielView in BodesuriView verschoben und SpielView entfernt.
    svn path=/trunk/; revision=1122

--- src/ui/spiel/BodesuriView.java
+++ src/ui/spiel/BodesuriView.java
@@ -1,22 +1,26 @@
 package ui.spiel;
 
-import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.Paint;
 import java.awt.TexturePaint;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
 import ui.ressourcen.Icons;
-import ui.spiel.brett.SpielView;
+import ui.spiel.brett.BrettView;
 import ui.spiel.chat.ChatView;
+import ui.spiel.karten.KartenAuswahlView;
 import applikation.client.controller.Steuerung;
 import applikation.client.pd.Spiel;
 
@@ -24,24 +28,34 @@ import applikation.client.pd.Spiel;
  * Das GUI des Spiels.
  */
 public class BodesuriView extends JFrame {
+	private static final int RAND = 8;
+
 	public BodesuriView(Steuerung steuerung, Spiel spiel) {
-		// Layout setzen
 		setTitle("Bodesuri - Spiel (" + spiel.spielerIch + ")");
 		setName("Bodesuri");
 		setLocationByPlatform(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setNativeLookAndFeel();
 
-		JPanel panel = new BodesuriViewPanel();
-		panel.setLayout(new BorderLayout());
-
 		// Views
-		SpielView spielView = new SpielView(steuerung, spiel);
+		JPanel topPanel = erstelleTopPanel(steuerung, spiel);
 		ChatView chatView = new ChatView(spiel.chat, steuerung);
 
-		// Layout zusammenstellen
-		panel.add(spielView, BorderLayout.NORTH);
-		panel.add(chatView, BorderLayout.CENTER);
+		// Layout
+		JPanel panel = new BodesuriViewPanel();
+		panel.setLayout(new GridBagLayout());
+
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = 0;
+		panel.add(topPanel, c);
+		panel.add(Box.createVerticalStrut(RAND), c);
+
+		c.fill = GridBagConstraints.BOTH;
+		c.weightx = 1.0;
+		c.weighty = 1.0;
+		panel.add(chatView, c);
+
+		panel.setBorder(BorderFactory.createEmptyBorder(RAND, RAND, RAND, RAND));
 
 		setContentPane(panel);
 
@@ -50,6 +64,24 @@ public class BodesuriView extends JFrame {
 //		JPanel jokerView = new JokerView(this);
 //		setGlassPane(jokerView);
 //		jokerView.setVisible(true);
+	}
+
+	private JPanel erstelleTopPanel(Steuerung steuerung, Spiel spiel) {
+		JPanel panel = new JPanel();
+		panel.setOpaque(false);
+		panel.setLayout(new GridBagLayout());
+
+		BrettView brettView = new BrettView(steuerung, spiel);
+		KartenAuswahlView kartenAuswahlView = new KartenAuswahlView(steuerung, spiel);
+
+		GridBagConstraints c = new GridBagConstraints();
+		c.anchor = GridBagConstraints.NORTH;
+		c.fill = GridBagConstraints.BOTH;
+
+		panel.add(brettView, c);
+		panel.add(kartenAuswahlView, c);
+
+		return panel;
 	}
 
 	/**
