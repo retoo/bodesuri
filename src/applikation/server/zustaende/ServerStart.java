4123500cc0cb9f465bf5797b59226194d7194b49
Reto Schüttel <reto@schuettel.ch>
2007-06-24 15:01:22 +0200

    - jaja.. cruisecontrol, hier ists
    svn path=/trunk/; revision=1196

--- src/applikation/server/zustaende/ServerStart.java
+++ src/applikation/server/zustaende/ServerStart.java
@@ -1,7 +1,5 @@
 package applikation.server.zustaende;
 
-import java.io.IOException;
-
 import dienste.automat.zustaende.PassiverZustand;
 import dienste.automat.zustaende.Zustand;
 import dienste.netzwerk.server.Server;
@@ -14,11 +12,7 @@ public class ServerStart extends ServerZustand implements PassiverZustand {
 	public Class<? extends Zustand> handle() {
 		SerialisierungsKontext kontext = spiel;
 
-		try {
-			spiel.server = new Server(spiel.queue, kontext);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
+		spiel.server = new Server(spiel.queue, kontext);
 
 		return EmpfangeSpieler.class;
 	}
