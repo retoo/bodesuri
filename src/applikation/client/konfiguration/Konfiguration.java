b0a202d93cb5ef52e4578d623cac720bbae9d8a0
Reto Schüttel <reto@schuettel.ch>
2007-06-24 23:01:51 +0200

    - Konfiguration nicht mehr Cloneable
    svn path=/trunk/; revision=1216

--- src/applikation/client/konfiguration/Konfiguration.java
+++ src/applikation/client/konfiguration/Konfiguration.java
@@ -1,6 +1,6 @@
 package applikation.client.konfiguration;
 
-public class Konfiguration implements Cloneable {
+public class Konfiguration {
 	/**
 	 * Debug-Option: Sollen es immer möglich sein Aufzugeben
 	 */
