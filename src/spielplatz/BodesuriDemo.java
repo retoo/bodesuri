fdb2442b0c89e46c76f697c6aada7c2434b28392
Robin Stocker <robin@nibor.org>
2007-06-25 19:58:07 +0200

    - Wieder 3 Bots in BodesuriDemo.
    svn path=/trunk/; revision=1246

--- src/spielplatz/BodesuriDemo.java
+++ src/spielplatz/BodesuriDemo.java
@@ -22,7 +22,7 @@ public class BodesuriDemo {
 		namen.add("Daisy");
 		namen.add("Mickey");
 
-		for (int i = 0; i < 2; i++) {
+		for (int i = 0; i < 3; i++) {
 			Konfiguration konfig = new Konfiguration();
 			konfig.defaultName = namen.get(i);
 
