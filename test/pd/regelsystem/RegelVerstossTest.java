5790a2f4efa541fe62497ea8eafad00bee2ed4e6
Pascal Hobus <pascal.hobus@schattenweiss.ch>
2007-06-27 21:14:35 +0200

    - JavaDoc
    svn path=/trunk/; revision=1309

--- test/pd/regelsystem/RegelVerstossTest.java
+++ test/pd/regelsystem/RegelVerstossTest.java
@@ -3,7 +3,14 @@ package pd.regelsystem;
 import pd.regelsystem.verstoesse.RegelVerstoss;
 import junit.framework.TestCase;
 
+/**
+ * Testet die Funktionalität eines Regelverstosses.
+ */
 public class RegelVerstossTest extends TestCase {
+	/**
+	 * Erstellt einen neuen Regelverstoss und prüft, ob dieser korrekt
+	 * erstellt und initialisiert wurde.
+	 */
 	public void testRegelVerstoss() {
 		RegelVerstoss rv = new RegelVerstoss("Des Todes!");
 		assertEquals("Regelverstoss: Des Todes!", rv.toString());
