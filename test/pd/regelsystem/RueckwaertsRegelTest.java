f3c4ad58d95b3fd70948d01ef5b801ddab1ac556
Pascal Hobus <pascal.hobus@schattenweiss.ch>
2007-06-27 21:30:53 +0200

    - JavaDoc
    svn path=/trunk/; revision=1310

--- test/pd/regelsystem/RueckwaertsRegelTest.java
+++ test/pd/regelsystem/RueckwaertsRegelTest.java
@@ -2,7 +2,17 @@ package pd.regelsystem;
 
 import pd.regelsystem.verstoesse.RegelVerstoss;
 
+/**
+ * Testet die Funktionalität der Rückwärtsregel an vielen verschiedenen
+ * Standorten, an der eine Figur stehen kann (Himmel, Bank, Lager, normales
+ * Feld).
+ */
 public class RueckwaertsRegelTest extends RegelTestCase {
+	/**
+	 * Validiert einen Rückwärtszug, dessen Ziel ein Bankfeld ist.
+	 * Bankfeld.
+	 * @throws RegelVerstoss
+	 */
 	public void testRueckwaerts() throws RegelVerstoss {
 		start = bank(0).getNtesFeld(5);
 		ziel  = bank(0);
@@ -10,6 +20,12 @@ public class RueckwaertsRegelTest extends RegelTestCase {
 		sollteValidieren(new RueckwaertsRegel(5));
 	}
 
+	/**
+	 * Validiert einen Rückwärtszug, dessen Start ein Bankfeld ist
+	 * und dessen Ziel vier Felder nach dem Bankfeld liegt. Dies
+	 * entspricht in Realität einem Vorwärtszug und gibt deshalb 
+	 * einen Verstoss.
+	 */
 	public void testRueckwaertsVorwaerts() {
 		start = bank(0);
 		ziel  = bank(0).getNtesFeld(5);
@@ -17,6 +33,11 @@ public class RueckwaertsRegelTest extends RegelTestCase {
 		sollteVerstossGeben(new RueckwaertsRegel(5));
 	}
 
+	/**
+	 * Validiert einen Rückwärtszug, dessen Start im Himmel ist und
+	 * dessen Ziel ebenfalls wieder im Himmel ist. Dies ist zwar eine
+	 * Rückwärtsbewegung, ist jedoch nicht gültig und gibt einen Verstoss.
+	 */
 	public void testRueckwaertsImHimmel() {
 		start = himmel(0, 3);
 		ziel  = himmel(0, 1);
@@ -24,6 +45,11 @@ public class RueckwaertsRegelTest extends RegelTestCase {
 		sollteVerstossGeben(new RueckwaertsRegel(2));
 	}
 
+	/**
+	 * Validiert einen Rückwärtszug, dessen Start im Himmel ist und dessen
+	 * Ziel das Bankfeld ist. Dies ist ein ungültiger Zug und gibt einen
+	 * Verstoss.
+	 */
 	public void testRueckwaertsAusHimmel() {
 		start = himmel(0).getNaechstes();
 		ziel  = bank(0);
@@ -31,6 +57,11 @@ public class RueckwaertsRegelTest extends RegelTestCase {
 		sollteVerstossGeben(new RueckwaertsRegel(2));
 	}
 
+	/**
+	 * Validiert einen Rückwärtszug, dessen Start ein Feld nach dem Bankfeld
+	 * ist und dessen Ziel ein Himmelfeld ist. Solch ein Zug ist ungültig
+	 * und gibt einen Verstoss.
+	 */
 	public void testRueckwaertsInHimmel() {
 		start = bank(0).getNaechstes();
 		ziel  = himmel(0);
@@ -38,12 +69,18 @@ public class RueckwaertsRegelTest extends RegelTestCase {
 		sollteVerstossGeben(new RueckwaertsRegel(2));
 	}
 
+	/**
+	 * Rückwärtszüge aus dem Lager heraus müssen auch einen Verstoss geben.
+	 */
 	public void testRueckwaertsAusLager() {
 		start = lager(0);
 		ziel  = bank(0);
 		sollteVerstossGeben(new RueckwaertsRegel(1));
 	}
 
+	/**
+	 * Rückwärtszüge in das Lager ergeben auch einen Verstoss.
+	 */
 	public void testRueckwaertsInLager() {
 		start = bank(0);
 		ziel  = lager(0);
