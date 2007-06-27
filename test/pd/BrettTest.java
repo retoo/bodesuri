d198771f0cc62d93a893b6b8199636f480b665a2
Pascal Hobus <pascal.hobus@schattenweiss.ch>
2007-06-27 20:18:42 +0200

    - JavaDoc
    svn path=/trunk/; revision=1299

--- test/pd/BrettTest.java
+++ test/pd/BrettTest.java
@@ -1,14 +1,24 @@
 package pd;
 
+/**
+ * Beinhaltet Tests für das Brett in der Problem-Domain.
+ */
 public class BrettTest extends ProblemDomainTestCase {
 	protected void setUp() {
 		super.setUp();
 	}
 	
+	/**
+	 * Prüft, ob alle Felder vorhanden sind.
+	 */
 	public void testGetAlleFelder() {
 		assertEquals(96, brett.getAlleFelder().size());
 	}
 	
+	/**
+	 * Prüft, ob Freies Lagerfeld gefunden wird und ob keines
+	 * gefunden wird, wenn alle Lagerfelder besetzt sind. 
+	 */
 	public void testGetFreiesLagerFeldVon() {
 		try {
 			brett.getFreiesLagerFeldVon(spieler(0));
