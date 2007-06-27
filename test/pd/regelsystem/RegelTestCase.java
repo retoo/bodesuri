f22e657ef9ba45734b1753541931d929ac2142a1
Pascal Hobus <pascal.hobus@schattenweiss.ch>
2007-06-27 21:01:17 +0200

    - JavaDoc
    svn path=/trunk/; revision=1306

--- test/pd/regelsystem/RegelTestCase.java
+++ test/pd/regelsystem/RegelTestCase.java
@@ -11,6 +11,11 @@ import pd.zugsystem.Bewegung;
 import pd.zugsystem.Zug;
 import pd.zugsystem.ZugEingabe;
 
+/**
+ * Ist Basis für alle TestCases des Regelsystems. Bietet zusätzlich zu
+ * den Initialisierungen von <Code>ProblemDomainTestCase</code> noch
+ * spezielle Instanzvariablen und Methoden für die Validierung.
+ */
 public abstract class RegelTestCase extends ProblemDomainTestCase {
 	protected Spieler spieler;
 	protected Feld start;
@@ -22,10 +27,23 @@ public abstract class RegelTestCase extends ProblemDomainTestCase {
 		spieler = spieler(0);
 	}
 	
+	/**
+	 * Validiert die standardmässig definierte Regel oder wirft
+	 * bei einem Fehlschlag eine Exception.
+	 * @throws RegelVerstoss
+	 */
 	protected void sollteValidieren() throws RegelVerstoss {
 		sollteValidieren(regel);
 	}
 	
+	/**
+	 * Validiert die angegebene Regel oder wirft bei einem Fehlschlag
+	 * eine Exception.
+	 * 
+	 * @param regel
+	 * 			zu validierende Regel.
+	 * @throws RegelVerstoss
+	 */
 	protected void sollteValidieren(Regel regel) throws RegelVerstoss {
 		if (regel == null) {
 			throw new RuntimeException("Keine Regel zum Validieren angegeben.");
@@ -35,10 +53,19 @@ public abstract class RegelTestCase extends ProblemDomainTestCase {
 		zug.ausfuehren();
     }
 	
+	/**
+	 * Prüft, ob Standardregel einen Verstoss verursacht.
+	 */
 	protected void sollteVerstossGeben() {
 		sollteVerstossGeben(regel);
 	}
 	
+	/**
+	 * Prüft, ob eine angegebene Regel einen Verstoss verursacht.
+	 * 
+	 * @param regel
+	 * 			Regel, die den Verstoss auslösen sollte.
+	 */
 	protected void sollteVerstossGeben(Regel regel) {
 		ZugEingabe ze = new ZugEingabe(spieler, null, getBewegungen());
         try {
@@ -49,6 +76,11 @@ public abstract class RegelTestCase extends ProblemDomainTestCase {
         }
 	}
 	
+	/**
+	 * Erstellt eine Liste mit eienr einzelnen Bewegung.
+	 * 
+	 * @return Liste mit einer Bewegungen.
+	 */
 	protected List<Bewegung> getBewegungen() {
 		Vector<Bewegung> bewegungen = new Vector<Bewegung>();
 		bewegungen.add(new Bewegung(start, ziel));
