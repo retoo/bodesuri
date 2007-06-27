6988dce7fabec29f7412df5371d61b576513271d
Pascal Hobus <pascal.hobus@schattenweiss.ch>
2007-06-27 22:24:36 +0200

    - JavaDoc
    svn path=/trunk/; revision=1312

--- test/pd/regelsystem/StartRegelTest.java
+++ test/pd/regelsystem/StartRegelTest.java
@@ -2,12 +2,21 @@ package pd.regelsystem;
 
 import pd.regelsystem.verstoesse.RegelVerstoss;
 
+/**
+ * Testet die Funktionalität der Startregel.
+ */
 public class StartRegelTest extends RegelTestCase {
 	protected void setUp() {
 		super.setUp();
 		regel = new StartRegel();
 	}
 	
+	/**
+	 * Prüft, ob die Startregel korrekt angewendet wird, und ob
+	 * danach normal mit der Figur gefahren werden kann.
+	 * 
+	 * @throws RegelVerstoss
+	 */
 	public void testStart() throws RegelVerstoss {
 		start = lager(0);
 		ziel  = bank(0);
@@ -25,12 +34,19 @@ public class StartRegelTest extends RegelTestCase {
 		assertFalse(ziel.istGeschuetzt());
 	}
 	
+	/**
+	 * Prüft, ob ein Start von einem Nicht-Lagerfeld unmöglich ist.
+	 */
 	public void testStartNichtVonLagerFeldAus() {
 		start = bank(0);
 		ziel  = bank(0).getNaechstes();
 		sollteVerstossGeben();
 	}
 	
+	/**
+	 * Prüft, ob die StartRegel einen Verstoss gibt, wenn keine Figur
+	 * auf dem Lagerfeld ist.
+	 */
 	public void testStartKeineFigurAufLagerFeld() {
 		start = lager(0);
 		ziel  = bank(0);
@@ -38,6 +54,10 @@ public class StartRegelTest extends RegelTestCase {
 		sollteVerstossGeben();
 	}
 	
+	/**
+	 * Prüft, ob die StartRegel einen Verstoss gibt, wenn keine Figur
+	 * auf dem Bankfeld ist.
+	 */
 	public void testStartNichtAufBankFeld() {
 		start = lager(0);
 		ziel  = bank(0).getNaechstes();
@@ -47,12 +67,22 @@ public class StartRegelTest extends RegelTestCase {
 		sollteVerstossGeben();
 	}
 	
+	/**
+	 * Prüft, ob andere Spieler auch starten können.
+	 */
 	public void testStartAndererSpieler() {
 		start = lager(1);
 		ziel  = bank(1);
 		sollteVerstossGeben();
 	}
 	
+	/**
+	 * Prüft, ob nach erfolgtem Start die Figur auch wirklich verschoben
+	 * wurde und ob das Feld geschützt ist. Ein erneuter Start ist dann
+	 * auf besetztes Feld nicht möglich.
+	 * 
+	 * @throws RegelVerstoss
+	 */
 	public void testStartGeschuetzt() throws RegelVerstoss {
 		start = lager(0);
 		ziel  = bank(0);
@@ -67,6 +97,22 @@ public class StartRegelTest extends RegelTestCase {
 		sollteVerstossGeben();
 	}
 	
+	/**
+	 * Prüft, ob nicht auf fremde Bankfelder gestartet werden kann.
+	 * 
+	 * @throws RegelVerstoss
+	 */
+	public void testStartAufFremdeBank() throws RegelVerstoss {
+		start = lager(0);
+		ziel  = bank(1);
+		sollteVerstossGeben();
+	}
+	
+	/**
+	 * Prüft, ob Figuren heimgeschickt werden, die auf der Bank stehen.
+	 * 
+	 * @throws RegelVerstoss
+	 */
 	public void testStartHeimSchicken() throws RegelVerstoss {
 		start = lager(0);
 		ziel  = bank(0);
