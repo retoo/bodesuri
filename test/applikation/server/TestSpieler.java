6ccd7c3ac94a1ddd80775329f2a6251ddd159dbf
Pascal Hobus <pascal.hobus@schattenweiss.ch>
2007-06-28 11:45:33 +0200

    - JavaDoc des JUnit-Tests für den TestSpieler, der im AutomatenTest gebraucht wird.
    svn path=/trunk/; revision=1328

--- test/applikation/server/TestSpieler.java
+++ test/applikation/server/TestSpieler.java
@@ -6,31 +6,64 @@ import dienste.netzwerk.EndPunktInterface;
 import dienste.netzwerk.Nachricht;
 import dienste.netzwerk.server.NetzwerkEvent;
 
+/**
+ * Stellt eine Hilfsklasse für den Test des Servers dar. Der TestSpieler
+ * simuliert einen Spieler und dessen Verhalten.
+ */
 public class TestSpieler {
 	public EndPunktInterface endpunkt;
+
 	public EventQueue eventQueue;
+
 	public String name;
 
-	public TestSpieler(String name, EndPunktInterface client, EventQueue eventQueue) {
+	/**
+	 * Erstellt einen neuen TestSpieler mit Namen, einem Endpunkt und einer
+	 * EventQueue.
+	 * 
+	 * @param name
+	 *            Name des Spielers
+	 * @param client
+	 *            Endpunkt für den Client
+	 * @param eventQueue
+	 *            EventQueue
+	 */
+	public TestSpieler(String name, EndPunktInterface client,
+			EventQueue eventQueue) {
 		this.name = name;
 		this.endpunkt = client;
 		this.eventQueue = eventQueue;
 	}
 
+	/**
+	 * Sendet eine neue SpielBeitreten-Nachricht an den Server im Namen des
+	 * Spielers.
+	 */
 	public void sendeBeitritt() {
 		sende(new SpielBeitreten(name));
 	}
 
+	/**
+	 * Sendet eine Nachricht im Namen des Spielers an den Server.
+	 * 
+	 * @param nachricht
+	 *            Zu sendende Nachricht.
+	 */
 	private void sende(Nachricht nachricht) {
 		endpunkt.sende(nachricht);
 	}
 
+	/**
+	 * @return Empfangene Nachricht.
+	 */
 	public Nachricht getNachricht() {
 		NetzwerkEvent netzwerkEvent = (NetzwerkEvent) eventQueue.dequeue();
 		return netzwerkEvent.brief.nachricht;
 	}
 
-
+	/**
+	 * @return Prüft, ob eine Nachricht empfangen wurde.
+	 */
 	public boolean hatNachrichten() {
 		return !eventQueue.isLeer();
 	}
