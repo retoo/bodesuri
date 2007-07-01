e39eb97cd98358ccfea59fe584922e5a94f302f9
Pascal Hobus <pascal.hobus@schattenweiss.ch>
2007-07-01 14:15:41 +0200

    - Refactoring: pd.regelsystem.Regelsystem mit static Methoden hinzugefügt
    svn path=/trunk/; revision=1371

---
+++ src/pd/regelsystem/Regelsystem.java
@@ -0,0 +1,35 @@
+package pd.regelsystem;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import pd.karten.Karte;
+import pd.spiel.spieler.Spieler;
+
+public class Regelsystem {
+	/**
+	 * @return true, wenn der Spieler mit seinen Karten ziehen kann
+	 */
+	public static boolean kannZiehen(Spieler spieler) {
+		for (Karte karte : spieler.getKarten()) {
+			Regel regel = karte.getRegel();
+			if (regel != null && regel.istZugMoeglich(spieler)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static List<ZugEingabe> getMoeglicheZuege(Spieler spieler) {
+		List<ZugEingabe> moegliche = new LinkedList<ZugEingabe>();
+
+		for (Karte karte : spieler.getKarten()) {
+			Regel regel = karte.getRegel();
+			if (regel != null) {
+				moegliche.addAll(regel.getMoeglicheZuege(spieler, karte));
+			}
+		}
+
+		return moegliche;
+    }
+}
