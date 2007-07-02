7c64b9106a04df7f8a4d55a97daa53f2acae1fdb
Philippe Eberli <p.eberli@gmx.ch>
2007-07-02 20:26:42 +0200

    - JavaDoc für SpielAbbruch
    svn path=/trunk/; revision=1399

--- src/applikation/nachrichten/SpielAbbruch.java
+++ src/applikation/nachrichten/SpielAbbruch.java
@@ -2,6 +2,10 @@ package applikation.nachrichten;
 
 import dienste.netzwerk.Nachricht;
 
+/**
+ * Nachricht mit der Server dem Client mitteilt, dass das Spiel abgebrochen
+ * werden soll.
+ */
 public class SpielAbbruch extends Nachricht {
 	public final String nachricht;
 
