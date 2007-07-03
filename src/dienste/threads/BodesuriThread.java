6139ce04e636d893e8c1ebc90f97bee12c725652
Reto Schüttel <reto@schuettel.ch>
2007-07-03 11:52:20 +0200

    - Thread javadocisiert
    svn path=/trunk/; revision=1439

--- src/dienste/threads/BodesuriThread.java
+++ src/dienste/threads/BodesuriThread.java
@@ -1,5 +1,14 @@
 package dienste.threads;
 
+/**
+ * Eine Bodesuri-angepasste Impelementation der Thread Klasse.
+ *
+ * Der Thread kommt von Haus aus mit einem eigenen exceptionHandler welcher bei
+ * Felern automatisch die Runtime-Umgebung beende. Dies ist notwendig da wir im
+ * Fehlerfall nicht garantieren können dsa alle Threads beendet sind.
+ * Unglücklicherweise beendet sich die Java Virtual Machine nur wenn alle
+ * Threads beendet sind.
+ */
 public class BodesuriThread extends Thread {
 	public BodesuriThread(String name) {
 		super(name);
