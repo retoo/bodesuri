94b5a9db05d176168ddde9ff321af43387b1e529
Pascal Hobus <pascal.hobus@schattenweiss.ch>
2007-07-04 17:22:44 +0200

    - Lizenz hinzugefügt (Source und Test)
    svn path=/trunk/; revision=1498

--- src/applikation/client/zugautomat/ZugAutomat.java
+++ src/applikation/client/zugautomat/ZugAutomat.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2007  Danilo Couto, Philippe Eberli,
+ *                     Pascal Hobus, Reto Schüttel, Robin Stocker
+ *
+ * This file is part of Bodesuri.
+ *
+ * Bodesuri is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Bodesuri is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bodesuri; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
 package applikation.client.zugautomat;
 
 import applikation.client.controller.Controller;
