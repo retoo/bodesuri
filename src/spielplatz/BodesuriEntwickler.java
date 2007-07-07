53d68c1c115385ba37a03e1886af81223a3c8dcd
Reto Schüttel <reto@schuettel.ch>
2007-07-08 01:20:40 +0200

    - lets try again (fixes #2)
    svn path=/trunk/; revision=1605

--- src/spielplatz/BodesuriEntwickler.java
+++ src/spielplatz/BodesuriEntwickler.java
@@ -25,7 +25,6 @@ import initialisierung.Bodesuri;
 import initialisierung.BodesuriServer;
 
 public class BodesuriEntwickler {
-
 	public static void main(String[] args) throws InterruptedException {
 		BodesuriServer server = new BodesuriServer(2);
 		server.start();
